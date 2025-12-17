f686d0db66f802f5fa04acb3fe167e5fa8aa8b33
KacaSas <katerina.saskova@gmail.com>
2025-12-17 16:20:37 +0100

    Change

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,6 +100,7 @@ with columna2:
 
 	clearTextArea = st.button('Clear the text area contents', key='clearTextArea', on_click=clearTextArea, use_container_width=True)
 
+
 st.write('<p style="margin-top: 3em;"><b><font style="font-size: 19px">Sources and references</font></b></p>', unsafe_allow_html=True)
 
 with st.expander('Sources and references', expanded=False):
